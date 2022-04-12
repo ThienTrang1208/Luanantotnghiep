aa51371d823e726eae5632cc19bbb61666c471c3
duynguyenhhg <nguyen.nguyen@hellohealthgroup.com>
2022-04-12 23:03:06 +0700

    logout

--- src/components/Giaodiennguoidung/Header.tsx
+++ src/components/Giaodiennguoidung/Header.tsx
@@ -1,27 +1,23 @@
-import React from "react";
-import "./Style-header.css";
-import Logo from "./assets/chusecret-kophong.png";
+import React from 'react'
+import './Style-header.css'
+import Logo from './assets/chusecret-kophong.png'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
     <div className="container">
-        <div className="header">
-                <div className="header__logo">
-                    <img
-                    src={Logo} alt="logo-1"
-                    />
-                </div>
-                <div className="header__search">
-                    <input type="text" placeholder="Tìm kiếm"/>
-                    <i className='bx bx-search-alt-2' ></i>
-                </div>
-                <div className="header__login">
-                   
-                </div>
-
+      <div className="header">
+        <div className="header__logo">
+          <img src={Logo} alt="logo-1" />
         </div>
+        <div className="header__search">
+          <input type="text" placeholder="Tìm kiếm" />
+          <i className="bx bx-search-alt-2"></i>
+        </div>
+        <div className="logout">
+          <Link to="/login">Đăng xuất</Link>
+        </div>
+      </div>
     </div>
-        
-   
-  );
+  )
 }
